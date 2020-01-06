e1d78eb314da047d636c12abb83c18615755d9ae
Vivian Coday <vivian@spanishowl.com>
2020-01-05 16:00:39 -0800

    Added confirmation messages

--- src/Treehouse.FitnessFrog/Controllers/EntriesController.cs
+++ src/Treehouse.FitnessFrog/Controllers/EntriesController.cs
@@ -56,6 +56,7 @@ namespace Treehouse.FitnessFrog.Controllers
             if (ModelState.IsValid)
             {
                 _entriesRepository.AddEntry(entry);
+                TempData["Message"] = "Your entry was successfully added!";
                 return RedirectToAction("Index");
             }
             SetupActivitiesSelectListItems();
@@ -83,6 +84,7 @@ namespace Treehouse.FitnessFrog.Controllers
             if (ModelState.IsValid)
             {
                 _entriesRepository.UpdateEntry(entry);
+                TempData["Message"] = "Your entry was successfully updated!";
                 return RedirectToAction("Index");
             }
             SetupActivitiesSelectListItems();
@@ -110,7 +112,7 @@ namespace Treehouse.FitnessFrog.Controllers
         public ActionResult Delete(int id)
         {
             _entriesRepository.DeleteEntry(id);
-
+            TempData["Message"] = "Your entry was successfully deleted!";
             return RedirectToAction("Index");
         }
         private void ValidateEntry(Entry entry)
